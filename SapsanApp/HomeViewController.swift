29a6953bd64c5e36bee1b5d542ffefe59931312b
meirinder <s.kulizhnikov@gmail.com>
2018-10-29 19:55:59 +0700

    Builded loginScreen

---
+++ SapsanApp/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  SapsanApp
+//
+//  Created by Savely on 29.10.2018.
+//  Copyright © 2018 Sapsan. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
