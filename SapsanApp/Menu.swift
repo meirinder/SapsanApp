dffc9bacd81b570baca645b269ab006d38b1e166
meirinder <s.kulizhnikov@gmail.com>
2018-11-04 18:48:52 +0700

    edit menu.swift

---
+++ SapsanApp/Menu.swift
@@ -0,0 +1,64 @@
+//
+//  Menu.swift
+//  SapsanApp
+//
+//  Created by Savely on 03.11.2018.
+//  Copyright © 2018 Sapsan. All rights reserved.
+//
+
+import UIKit
+
+class Menu: UIViewController {
+
+    
+       var menuVC :MenuViewController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        menuVC =  (self.storyboard?.instantiateViewController(withIdentifier: "MenuVC") as! MenuViewController)
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(self.handleSwipe))
+        swipeRight.direction = .right
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(self.handleSwipe))
+        swipeLeft.direction = .left
+        
+        self.view.addGestureRecognizer(swipeRight)
+        self.view.addGestureRecognizer(swipeLeft)
+    }
+    
+
+    @objc func handleSwipe(gesture : UISwipeGestureRecognizer){
+        switch gesture.direction {
+        case UISwipeGestureRecognizer.Direction.right:
+            showMenu()
+            break
+        case UISwipeGestureRecognizer.Direction.left:
+            hideMenu()
+            break
+        default: break
+            
+        }
+    }
+    
+    func showMenu() {
+        UIView.animate(withDuration: 0.3){
+            self.menuVC.view.frame = CGRect(x: 0, y: 60, width: UIScreen.main.bounds.size.width,
+                                            height: UIScreen.main.bounds.size.height)
+            self.addChild(self.menuVC)
+            self.view.addSubview(self.menuVC.view)
+            AppDelegate.isMenuVC = false
+        }
+    }
+    
+    func hideMenu() {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.menuVC.view.frame = CGRect(x: -UIScreen.main.bounds.size.width, y: 60,
+                                            width: UIScreen.main.bounds.size.width,
+                                            height: UIScreen.main.bounds.size.height)
+            
+        }){ (finished) in self.menuVC.view.removeFromSuperview()
+            AppDelegate.isMenuVC = true
+        }
+    }
+}
